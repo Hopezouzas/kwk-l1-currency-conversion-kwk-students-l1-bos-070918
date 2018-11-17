71a1020baf2849a1f1cb7646eb9bc959ab1950e2
Hope Zouzas <hmzouzas@gmail.com>
2018-11-17 01:14:47 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,8 +1,8 @@
 # Write your cod here
 def usd_to_eur
-  expect(eur_to_usd(usd_to_eur(1.0))).to eql(1.5)
+  expect(eur_to_usd(usd_to_eur(1.0))).to eql(1.)
 end
 
 def eur_to_usd
-  expect(eur_to_usd(usd_to_eur(1.5))).to eql(1.0)
+  expect(eur_to_usd(usd_to_eur(1.0))).to eql(1.0)
 end
