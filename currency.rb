838dad3dbeaeb400d76d2fbbb2be310e37fa3a59
Hope Zouzas <hmzouzas@gmail.com>
2018-11-17 01:16:32 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,5 +1,7 @@
 # Write your cod here
 def usd_to_eur
+  def eur_to_usd
  it "should convert dollars to euros and back" do
     expect(eur_to_usd(usd_to_eur(1.0))).to eql(1.0), "Dollars (usd) did not correctly convert to euros (eur) and back to dollars"
 end
+end 
